fbd605992dda1ac99a3df4ea396e46450fcbdf22
Λυδяэφ ♫ <gueguen.audrey@yahoo.fr>
2025-01-24 10:49:56 +0100

    Backend Java Springboot initialization

---
+++ backend_GdsManager/src/main/java/com/grainedesabot/gdsmanager/GdsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.grainedesabot.gdsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdsManagerApplication.class, args);
+	}
+
+}
